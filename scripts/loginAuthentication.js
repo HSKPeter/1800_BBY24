755e6a4055b56e6af3f907ebfa712175d4960201
Peter Hui <huishikuen@gmail.com>
2021-11-23 20:02:03 -0800

    removed redundant parts

--- scripts/loginAuthentication.js
+++ scripts/loginAuthentication.js
@@ -1,15 +1,7 @@
 firebase.auth().onAuthStateChanged(user => {
-    console.log(user)
+    // console.log(user)
     db.collection("users").doc(user.uid).collection("timerSessions").doc("test").set()
     if (!user) {
         window.location.replace("/login.html");
     }
 });
-
-// firebase.auth().onAuthStateChanged(user => {
-//     console.log("User")
-//     console.log(user)
-//     if (!user) {
-//         window.location.replace("/login.html");
-//     }
-// });
