1c963ca244310a9c264cc8a0044bf6cacaa2e531
Daniel Shen <dshen20@my.bcit.ca>
2021-11-25 10:11:01 -0800

    tried some things in settings.html and settings.js

--- scripts/settings.js
+++ scripts/settings.js
@@ -2,6 +2,8 @@ function toggleColorTheme(){
     var currentTheme = db.collection("users").doc(user.uid);
     if (currentTheme.data.theme == "dark") {
         currentTheme.data.theme = "light";
+        document.getElementsByClassName("bg-primary-color-scheme").style.background-color;
+
     }
     if (currentTheme == light) {
         currentTheme.data.theme = "dark";
