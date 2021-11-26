da95038043b1da313529919cb51f1ae701d7bd4f
Peter Hui <huishikuen@gmail.com>
2021-11-26 12:31:49 -0800

    refactored and commented code

--- scripts/timer/assumeStartTime.js
+++
@@ -1,5 +0,0 @@
-function assumeStartTime(progress, sessionLengthInMs){
-    const current = new Date().getTime();
-    const msPassed = sessionLengthInMs * progress;
-    return current - msPassed;
-}
