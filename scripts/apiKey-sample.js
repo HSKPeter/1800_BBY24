11ccfdc459cedc7f917119a47b447a06af56258a
Peter Hui <huishikuen@gmail.com>
2021-12-01 22:12:21 -0800

    created a sample of  apiKey.js

---
+++ scripts/apiKey-sample.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
